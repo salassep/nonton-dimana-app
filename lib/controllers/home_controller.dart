e022c5b7569a670f1beb53e51a7b47a8c8802c3e
Salas Sepkardianto <66544116+salassep@users.noreply.github.com>
2023-07-29 23:33:07 +0800

    Resize only when textform focus or filled

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -4,11 +4,19 @@ import 'package:nonton_dimana_app/services/movie_service.dart';
 
 class HomeController extends GetxController {
   final TextEditingController titleController = TextEditingController();
+  final FocusNode focusNode = FocusNode();
   // final ApiService _apiService = ApiService();
   final MovieService _movieService = MovieService();
 
   // RxList movies = [].obs;
   // RxString title = ''.obs;
+
+  RxBool get isActive {
+    if (focusNode.hasFocus || titleController.text.isNotEmpty) {
+      return true.obs;
+    } 
+    return false.obs;
+  }
 
   List searchMovieByTitle(String value) {
     // final result = await _apiService.getMoviesByTitle(title, 'id');
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,104 +5,119 @@ import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
 class Home extends GetView<HomeController>{
-  Home({Key? key}) : super(key: key);
-
-  final HomeController homeController = Get.put(HomeController());
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final screenHeight = MediaQuery.of(context).size.height;
+    final HomeController homeController = Get.put(HomeController());
+    final bool isKeyboardOpen = MediaQuery.of(context).viewInsets.bottom > 0;
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.only(top: screenHeight / 6, left: 20.0, right: 20.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: Image.asset(
-                  'assets/images/logo-cut.png'
+      extendBodyBehindAppBar: true,
+      resizeToAvoidBottomInset: false,
+      body: GestureDetector(
+        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+        child: Obx(() => Padding(
+            padding: homeController.isActive.value || isKeyboardOpen
+              ? const EdgeInsets.only(left: 20.0, right: 20.0, top: 50.0) 
+              : const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: homeController.isActive.value || isKeyboardOpen
+                ? MainAxisAlignment.start 
+                : MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(40.0),
+                  child: Image.asset(
+                    'assets/images/logo-cut.png'
+                  ),
                 ),
-              ),
-              const SizedBox(height: 10.0),
-              TypeAheadField(
-                animationStart: 0,
-                animationDuration: Duration.zero,
-                textFieldConfiguration: const TextFieldConfiguration(
-                  autofocus: true,
-                  decoration: InputDecoration(
-                    hintText: 'Apa judul filmnya ?',
-                    prefixIcon: Icon(Icons.search),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFFB3005E))
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                TypeAheadField(
+                  animationStart: 0,
+                  animationDuration: Duration.zero,
+                  textFieldConfiguration: TextFieldConfiguration(
+                    controller: homeController.titleController,
+                    focusNode: homeController.focusNode,
+                    decoration: const InputDecoration(
+                      hintText: 'Apa judul filmnya ?',
+                      prefixIcon: Icon(Icons.search),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFB3005E))
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                      )
                     )
-                  )
+                  ),
+                  hideSuggestionsOnKeyboardHide: false,
+                  suggestionsCallback: (value) {
+                    if (value.isEmpty) {
+                      return [];
+                    }
+                    return homeController.searchMovieByTitle(value);
+                  },
+                  itemBuilder: (BuildContext context, value) {
+                    return Card(
+                      child: Row(
+                        children: [
+                          CachedNetworkImage(
+                            imageUrl: value['poster'],
+                            imageBuilder: (context, imageProvider) {
+                              return Container(
+                                width: 140,
+                                height: 200,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: imageProvider,
+                                    fit: BoxFit.fill,
+                                  )
+                                ),
+                              );
+                            },
+                            placeholder: (context, url) {
+                              return Container(
+                                width: 140,
+                                height: 200,
+                                color: Colors.grey,
+                                child: Image.asset(
+                                  'assets/images/logo-no-word.png'
+                                ),
+                              );
+                            },
+                            errorWidget: (context, url, error) {
+                              return Container(
+                                width: 140,
+                                height: 200,
+                                color: Colors.grey,
+                                child: Image.asset(
+                                  'assets/images/logo-no-word.png'
+                                ),
+                              );
+                            }
+                          ),
+                          const SizedBox(width: 15.0),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Judul: ${value['title']}',
+                                  overflow: TextOverflow.fade,
+                                ),
+                                Text('Tahun: ${value['year']}'),
+                              ],
+                            ),
+                          ),
+                        ],
+                      )
+                    );
+                  }, 
+                  onSuggestionSelected: (suggestion) {
+                    print(suggestion);
+                  }
                 ),
-                hideSuggestionsOnKeyboardHide: false,
-                suggestionsCallback: (value) {
-                  if (value.isEmpty) return [];
-                  return homeController.searchMovieByTitle(value);
-                },
-                itemBuilder: (context, value) {
-                  return Card(
-                    color: const Color(0xFFB3005E),
-                    child: Row(
-                      children: [
-                        CachedNetworkImage(
-                          imageUrl: value['poster'],
-                          imageBuilder: (context, imageProvider) {
-                            return Container(
-                              width: 140,
-                              height: 200,
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: imageProvider,
-                                  fit: BoxFit.fill,
-                                )
-                              ),
-                            );
-                          },
-                          placeholder: (coantext, url) {
-                            return Container(
-                              width: 140,
-                              height: 200,
-                              color: Colors.grey,
-                              child: Image.asset(
-                                'assets/images/logo-no-word.png'
-                              ),
-                            );
-                          },
-                          errorWidget: (context, url, error) {
-                            return Container(
-                              width: 140,
-                              height: 200,
-                              color: Colors.grey,
-                              child: Image.asset(
-                                'assets/images/logo-no-word.png'
-                              ),
-                            );
-                          }
-                        ),
-                        SizedBox(width: 15.0),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Judul: ${value['title']}'),
-                            Text('Tahun: ${value['year']}'),
-                          ],
-                        ),
-                      ],
-                    )
-                  );
-                }, 
-                onSuggestionSelected: (suggestion) {
-                  print(suggestion);
-                }
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
